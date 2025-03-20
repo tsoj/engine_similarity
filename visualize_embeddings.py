493646e6ef663d7bd222866df5343b4b57515f80
Jost Triller <tsoj.tsoj@gmx.de>
2025-03-20 20:16:23 +0100

    Added monty

--- visualize_embeddings.py
+++ visualize_embeddings.py
@@ -160,11 +160,11 @@ def analyze_and_visualize_similarity_matrix(similarity_matrix, labels, output_gr
     pos = nx.spring_layout(G_layout, weight='weight', k=0.4, iterations=100, seed=42)
 
     cluster_colors = [
-        (0.17254901960784313, 0.6274509803921569,  0.17254901960784313 ),  # 2ca02c green
+        (1.0,                 0.4980392156862745,  0.054901960784313725),  # ff7f0e orange
         (0.8392156862745098,  0.15294117647058825, 0.1568627450980392  ),  # d62728 red
         (0.5803921568627451,  0.403921568627451,   0.7411764705882353  ),  # 9467bd purple
         (0.238, 0.544,  0.789  ),                                          # 3d8bc9 light blue
-        (1.0,                 0.4980392156862745,  0.054901960784313725),  # ff7f0e orange
+        (0.17254901960784313, 0.6274509803921569,  0.17254901960784313 ),  # 2ca02c green
         (0.7372549019607844,  0.7411764705882353,  0.13333333333333333 ),  # bcbd22 yellow
         (0.4980392156862745,  0.4980392156862745,  0.4980392156862745  ),  # 7f7f7f grey
         (0.09019607843137255, 0.7450980392156863,  0.8117647058823529),    # 17becf türkis
@@ -175,7 +175,7 @@ def analyze_and_visualize_similarity_matrix(similarity_matrix, labels, output_gr
 
     node_colors = [cluster_colors[cl % len(cluster_colors)] for cl in cluster_labels]
     for n1, n2 in G.edges():
-        alpha = G[n1][n2]['weight'] ** 4.0
+        alpha = G[n1][n2]['weight'] ** 5.0
         width = max(0.8, alpha * 15.0)
         color1 = node_colors[n1]
         color2 = node_colors[n2]
